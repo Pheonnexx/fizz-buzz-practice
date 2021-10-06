d8ecb0e982a20a21766e22ad2db8dd8bffc3d628
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-10-06 11:46:37 +0100

    numbers divisible by 15 return FizzBuzz

--- fizzbuzz/__init__.py
+++ fizzbuzz/__init__.py
@@ -1,6 +1,9 @@
 def fizz_buzz_output(number):
-    if number % 3 == 0:
+    if number % 15 == 0:
+        return "FizzBuzz"
+    elif number % 3 == 0:
         return "Fizz"
     elif number % 5 == 0:
         return "Buzz"
+
     return "Not Fizz"
--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -5,22 +5,29 @@ from fizzbuzz import fizz_buzz_output
 
 class TestFizzBuzzOutput(unittest.TestCase):
     def test_divisible_by_three(self):
-        # when divisible by three then Fizz is returned.
+        # When divisible by three then Fizz is returned.
         result = fizz_buzz_output(9)
 
         self.assertEqual(result, "Fizz")
 
     def test_not_divisible_by_three_or_five(self):
-        # when divisible by three then Fizz is returned.
+        # When not divisible by three or five then Not Fizz is returned.
         result = fizz_buzz_output(8)
 
         self.assertEqual(result, "Not Fizz")
     
     def test_divisible_by_five(self):
-        # when divisible by five then Buzz is returned.
+        # When divisible by five then Buzz is returned.
         result = fizz_buzz_output(10)
 
         self.assertEqual(result, "Buzz")
+
+    def test_divisible_by_fifteen(self):
+        # When divisible by 15 FizzBuzz is returned.
+        result = fizz_buzz_output(15)
+
+        self.assertEqual(result, "FizzBuzz")
+
 
 if __name__ == '__main__':
     unittest.main()
