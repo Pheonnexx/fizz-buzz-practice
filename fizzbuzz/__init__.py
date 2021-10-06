e4f4c44330c06d7ce8e34541887b2e787565f09a
Heather Poole <heather.poole@digital.justice.gov.uk>
2021-10-06 11:08:10 +0100

    numbers divisible by 5 return buzz

--- fizzbuzz/__init__.py
+++ fizzbuzz/__init__.py
@@ -1,4 +1,6 @@
 def fizz_buzz_output(number):
     if number % 3 == 0:
         return "Fizz"
+    elif number % 5 == 0:
+        return "Buzz"
     return "Not Fizz"
--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -10,11 +10,17 @@ class TestFizzBuzzOutput(unittest.TestCase):
 
         self.assertEqual(result, "Fizz")
 
-    def test_not_divisible_by_three(self):
+    def test_not_divisible_by_three_or_five(self):
         # when divisible by three then Fizz is returned.
         result = fizz_buzz_output(8)
 
         self.assertEqual(result, "Not Fizz")
+    
+    def test_divisible_by_five(self):
+        # when divisible by five then Buzz is returned.
+        result = fizz_buzz_output(10)
+
+        self.assertEqual(result, "Buzz")
 
 if __name__ == '__main__':
     unittest.main()
